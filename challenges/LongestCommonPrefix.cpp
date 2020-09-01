3b003c7c9c844d788945cc02f1643c9c2aadc94b
ChadMcIntire <chadmcintirecloud@gmail.com>
2020-09-01 14:41:47 -0600

    Cleaned up LongestCommonPrefix

--- challenges/LongestCommonPrefix.cpp
+++ challenges/LongestCommonPrefix.cpp
@@ -19,50 +19,24 @@ public:
         }
     }
 
-    if (minSize == 0){
-        return "";
-    }
-
-    if (minSize == 1 && strs.size() == 1){
-        return strs[0];
-    }
-
-
-    char firstChar = strs.front()[0];
-    if (minSize == 1 && strs.size() > 1){
-        for(string i : strs) {
-            if (i[0] != firstChar){
-                return "";
-            }
-        }
-
-        string convChar(1, firstChar);
-        return convChar;
-    }
-
     bool stop = true;
     string final = "";
     for (int it=0; it<minSize; it++)
     {
-        char current_string = strs.front()[it]; 
+        char current_string = strs.front()[it];
         for(string i : strs) {
             if (i[it] != current_string)
             {
                 stop = false;
                 break;
             }
-            if (minSize == 1)
-            {
-                break;
-            }
         }
-        if (stop != false){
+        if (stop == true){
             final += current_string;
         }
         else{
             break;
         }
-
     }
 
     return final;
